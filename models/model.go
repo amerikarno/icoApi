ae18c9173cdb9c0f2789e55475e7d47d1d84a4bf
karnake ruengchaicharnkij <karnakeruengchaicharnkij@karnakes-MacBook-Pro.local>
2023-11-14 17:20:50 +0700

    add more fields in IDCardOpenAccounts struct

--- models/model.go
+++ models/model.go
@@ -56,10 +56,21 @@ type TambonList struct {
 
 type IDCardOpenAccounts struct {
 	AccountID      string `json:"id" gorm:"column:id"`
+	ThTitle        string `json:"thTitle" gorm:"column:th_title"`
+	ThName         string `json:"thName" gorm:"column:th_name"`
+	ThSurname      string `json:"thSurname" gorm:"column:th_surname"`
+	EngTitle       string `json:"engTitle" gorm:"column:en_title"`
+	EngName        string `json:"engName" gorm:"column:en_name"`
+	EngSurname     string `json:"engSurname" gorm:"column:en_surname"`
+	Email          string `json:"email" gorm:"column:email"`
+	Mobile         string `json:"mobile" gorm:"column:mobile_no"`
+	Agreement      string `json:"agreement" gorm:"column:personal_agreement"`
 	BirthDate      string `json:"birthDate" gorm:"column:birth_date"`
 	MarriageStatus string `json:"marriageStatus" gorm:"column:marriage_status"`
 	IDCard         string `json:"idCard" gorm:"column:id_card"`
 	LaserCode      string `json:"laserCode" gorm:"column:laser_code"`
+	Pages          string `json:"pages" gorm:"column:personal_pages"`
+	Create         string `json:"create" gorm:"column:create_at"`
 }
 
 func (IDCardOpenAccounts) TableName() string { return "personal_informations" }
