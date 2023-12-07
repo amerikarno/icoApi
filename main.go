8cfb42ab3583b0b20ff201b422b763c1db9e57ca
Karnake Ruengchaicharnkij <karnake.r@finansiada.com>
2023-12-07 09:24:12 +0700

    merge 202312070924

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"crypto/tls"
 	"log"
-	"net/http"
 
 	"github.com/amerikarno/icoApi/external"
 	"github.com/amerikarno/icoApi/handlers"
 	"github.com/amerikarno/icoApi/repository"
 	"github.com/amerikarno/icoApi/usecases"
 	"github.com/labstack/echo/v4"
+
 	// "golang.org/x/crypto/acme/autocert"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -38,16 +37,17 @@ func main() {
 	e.POST("api/v1/idcard", handler.PostIDcard())
 	e.POST("api/v1/personalInformation", handler.PostPersonalInformations())
 
-	server := http.Server{
-		Addr: ":1323",
-		Handler: e,
-		TLSConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	}
+	// server := http.Server{
+	// 	Addr: ":1323",
+	// 	Handler: e,
+	// 	TLSConfig: &tls.Config{
+	// 		InsecureSkipVerify: true,
+	// 	},
+	// }
 
-	if err := server.ListenAndServeTLS("./cert/myCA.pem", "./cert/myCA.crt"); err != nil {log.Fatal(err)}
+	// if err := server.ListenAndServeTLS("./cert/myCA.pem", "./cert/myCA.crt"); err != nil {log.Fatal(err)}
 	// e.Logger.Fatal(e.StartTLS(":1323", "./cert/myCA.pem", "./cert/myCA.crt" ))
+	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func initOpenAccountsDB() *gorm.DB {
