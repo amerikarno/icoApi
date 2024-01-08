e75203155c3361abd1d5c34579c48a206c2195df
Karnake Ruengchaicharnkij <karnake.r@finansiada.com>
2024-01-08 14:56:07 +0700

    confirm email

--- test/main.go
+++ test/main.go
@@ -1,65 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
-
-	"github.com/amerikarno/icoApi/external"
-	"github.com/amerikarno/icoApi/handlers"
-	"github.com/amerikarno/icoApi/repository"
-	"github.com/amerikarno/icoApi/usecases"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"time"
 )
 
 func main() {
-	filename := "json/api_province_with_amphure_tambon.json"
-	repo := repository.NewPATRepository(filename)
-	provinces, amphures, tambons, zipcode := repo.LoadPAT().GetProvinceAmphureTambonLists()
-
-	openAccountsDB := initOpenAccountsDB()
-	openAccountsRepo := repository.NewOpenAccountsRepository(openAccountsDB)
-
-	external := external.NewExternalUuid()
-	e := echo.New()
-
-	e.Use(middleware.KeyAuth(func(key string, c echo.Context) (bool, error) {
-		return key == "fda-authen-key", nil
-	}))
-
-	usecase := usecases.NewOpenAccountUsecases(openAccountsRepo, external)
-	handler := handlers.NewHandler(usecase)
-	e.GET("verify/email/:email/mobile/:mobileno", handler.VerifyEmailMobileHandler())
-	e.GET("verify/email/:email", handler.VerifyEmailHandler())
-	e.GET("verify/mobile/:mobileno", handler.VerifyMobileNoHandler())
-	e.GET("verify/idcard/:idcard", handler.VerifyIDCardHandler())
-	e.GET("api/v1/all_provinces", handler.GetAllProvinces(provinces))
-	e.GET("api/v1/amphures/:province", handler.GetAmphuresInProvince(amphures))
-	e.GET("api/v1/tambons/:amphure", handler.GetTambonsInAmphure(tambons))
-	e.GET("api/v1/zipcode/:zipname", handler.GetZipCode(zipcode))
-	e.GET("api/v1/idcard/:idcard", handler.GetIDcard())
-	e.POST("api/v1/idcard", handler.PostIDcard())
-	e.POST("api/v1/personalInformation", handler.PostPersonalInformations())
-
-	// server := http.Server{
-	// 	Addr: ":1323",
-	// 	Handler: e,
-	// 	TLSConfig: &tls.Config{
-	// 		InsecureSkipVerify: true,
-	// 	},
-	// }
-
-	// if err := server.ListenAndServeTLS("./cert/myCA.pem", "./cert/myCA.crt"); err != nil {log.Fatal(err)}
-	// e.Logger.Fatal(e.StartTLS(":1323", "./cert/myCA.pem", "./cert/myCA.crt" ))
-	e.Logger.Fatal(e.Start(":1323"))
+	t1 := 5
+	test := fmt.Sprintf("test = %d%%", t1)
+	log.Println(test)
+	log.Println(convertDateInThai(time.Now().Local()))
 }
 
-func initOpenAccountsDB() *gorm.DB {
-	dsn := "root:password@tcp(127.0.0.1:3306)/open_accounts?charset=utf8mb4&parseTime=True&loc=Local"
-	mysqldb, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
+func convertDateInThai(d time.Time) (thaiDate string) {
+	var monthStr string
+	date := d.Day()
+	month := int(d.Month())
+	year := d.Year() + 543
+	switch month {
+	case 1:
+		monthStr = "มกราคม"
+	case 2:
+		monthStr = "กุมภาพันธ์"
+	case 3:
+		monthStr = "มีนาคม"
+	case 4:
+		monthStr = "เมษายน"
+	case 5:
+		monthStr = "พฤษภาคม"
+	case 6:
+		monthStr = "มิถุนายม"
+	case 7:
+		monthStr = "กรกฎาคม"
+	case 8:
+		monthStr = "สิงหาคม"
+	case 9:
+		monthStr = "กันยายน"
+	case 10:
+		monthStr = "ตุลาคม"
+	case 11:
+		monthStr = "พฤษจิกายน"
+	case 12:
+		monthStr = "ธันวาคม"
 	}
-	return mysqldb
+	return fmt.Sprintf("%d %s %d", date, monthStr, year)
 }
